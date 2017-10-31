b356f07a04c16eeaf152bc5be0ec418670d52891
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.wv.cc.cmu.edu>
2017-10-31 12:14:07 -0400

    working

--- speakextension/scripts/imageLabeler.js
+++ speakextension/scripts/imageLabeler.js
@@ -1,4 +1,5 @@
-var altText = "";
+var totalImagesOnScreen = 0;
+var altTextArray = [];
 
 $(document).ready(function() {
 	//labelImage();
@@ -7,49 +8,111 @@ $(document).ready(function() {
 
 function labelImage() {
 	$('img').each(function() {
-		var alttext = $(this).attr("alt");
-		var imgsrc  = $(this).attr("src");
-		var imageContext = $(this);
-
 		$.ajax({
 		    url: "http://www.accessibilitycourse.com/support/rekognition.php?",
 
 		 	data: {
 		    	url: $(this).attr("src")
 		    },
-		 
+		 	context:this,
 		 	success: function( response ) {
-		    	var response = JSON.parse(response);
-		    	console.log("ajax")
-		    	altText = response[0].Name + ", " + response[1].Name + ", " + response[2].Name;
-		    	console.log(altText)
-		    }
+		 		if(response != undefined) {
+		 			var altText = $(this).attr("alt");
+
+		    		var response = JSON.parse(response);
+		    		if(response[2] != undefined) {
+		    			var newAlt = response[0].Name + ", " + response[1].Name + ", " + response[2].Name;
+		    		}
+		    		else if (response[1] != undefined) {
+		    			var newAlt = response[0].Name + ", " + response[1].Name
+		    		}
+		    		else {
+		    			var newAlt = response[0].Name
+		    		}
+		    		console.log(newAlt)
+
+		    		//image does not have alt text
+	  				if(altText == undefined) { 
+	  					console.log(" image : " + $(this))
+	  					console.log(newAlt)
+	  					$().attr("alt", newAlt)
+	  				} 
+	  				//image has empty alt text
+	  				else if(altText == "") {
+	  					console.log("empty alt") 
+	  					$(this).attr("alt", newAlt)
+	  				} 
+		    	}
+			}
 		});
-
-		//image does not have alt text
-	  	if(alttext && (alttext == "" || alttext == undefined) || alttext == '\n' || alttext == null) { 
-	  		console.log("no alt")
-	  		$(this).setAttribute('alt', altText)
-	  	} 
-	  	//image has empty alt text
-	  	else if($(this).attr('alt')) {
-	  		console.log("empty alt") 
-	  		$(this).attr('alt', altText)
-	  	} 
-
-		console.log("not ajax")
-		console.log(altText);
 	})
 }
 
 function labelImageTesseract() {
-	$('img').each(function() {
-		Tesseract.recognize($(this).attr("src"))
+	//$('img').each(function() {
+	for(var i=0; i<getNumberImages(); i++) {
+		//var altText = $(this).attr("alt");
+		var altText = $($("img")[i]).attr("alt")
+		var image = $($("img")[i])
+		console.log("aaaa" + image);
+		
+
+		Tesseract.recognize($($("img")[i]).attr("src"))
+		//Tesseract.recognize($(this).attr("src"))
     	.progress(message => console.log(message))
     	.catch(err => console.error(err))
     	.then(result => console.log(result))
     	.finally(function(resultOrError) {
-      		console.log(resultOrError);
-    	})    		
-	})	
+    		newAlt = resultOrError.text;
+    		console.log(newAlt);
+
+    		altTextArray.push(newAlt);
+    		console.log("aroon : " + altTextArray);
+
+    		for(var j=0; j<i; j++) {
+    			($($("img")[j])).attr("alt", altTextArray[j]);
+    		}
+
+    		//console.log($($("img")[i]))
+		    //image does not have alt text
+	  		if(altText == undefined) { 
+	  			//console.log("no alt");
+	  			//image.attr("alt", altTextArray[i])
+	  		} 
+	  		//image has empty alt text
+	  		else if(altText == "") {
+	  			//console.log("empty alt");
+	  			//console.log("aroon  ...... "+image)
+	  			//image.attr("alt", altTextArray[i])
+	  		}  
+	  		console.log(image.src);
+	  		image.attr("alt", altTextArray[i]);
+    	})    	
+	}	
+}
+
+
+function tessaTessaTesseract() {
+
+	for(var i=0; i<totalImagesOnScreen; i++) {
+		Tesseract.recognize($($("img")[i]).attr("src"))
+    	.progress(message => console.log(message))
+    	.catch(err => console.error(err))
+    	.then(result => console.log(result))
+    	.finally(function(resultOrError) {
+	    	altText = resultOrError.text;   
+	    	altText = altTextReadFromImage.toString();
+			altText = altTextReadFromImage.trim();  
+			altTextReadFromImageList.push(altText);     	
+    	})
+	}	
+}
+
+
+function getNumberImages() {
+	if(document.body.getElementsByTagName("img")) {
+		var elems = document.body.getElementsByTagName("img");
+		totalImagesOnScreen = elems.length;
+	}
+	return totalImagesOnScreen;
 }
